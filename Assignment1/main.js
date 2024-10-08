bcb3e13003e6937e1958dfcdadd4069e978b1dbe
JesseMelon <Jessechambers2000@icloud.com>
2024-10-08 19:20:15 -0400

    engine dedicated file and lab5

--- Assignment1/main.js
+++ Assignment1/main.js
@@ -1,11 +1,9 @@
 // Define vertices, colors, and indices for a cube
 const vertices = new Float32Array([
-    // Front face
     -1, -1,  1, // 0
      1, -1,  1, // 1
      1,  1,  1, // 2
     -1,  1,  1, // 3
-    // Back face
     -1, -1, -1, // 4
      1, -1, -1, // 5
      1,  1, -1, // 6
@@ -38,32 +36,34 @@ const indices = new Uint16Array([
     4, 0, 3, 4, 3, 7,
 ]);
 
-// Create WebGL context and initialize the cube
+// Create WebGL context
 const canvas = document.querySelector('canvas');
+
+//TODO should maybe make this a function in MelonEngine
 const gl = initCanvas(canvas);
-const cubeMesh = new Mesh(gl, vertices, colors, indices);
-
-// Create a ProgramInstance
-const programInstance = new ProgramInstance(gl); //default invoked
-// Create the renderer and register the cube instance
-const rendererI = new renderer(gl);
-
-// Create a MeshInstance for the cube
-const cubeInstance = new MeshInstance(cubeMesh, programInstance, rendererI, [0, 0, 0], [0, 0, 0], [1, 1, 1]);
-
-const camera = new Camera();
-
+//TODO use gl.viewport for dynamic sizing or something (seems easy)
 gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 gl.enable(gl.DEPTH_TEST);
 gl.enable(gl.CULL_FACE);
 gl.cullFace(gl.BACK);
+gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+//one renderer for one scene, one camera for render for this usage
+const renderer = new MelonEngine.Renderer(gl);
+const camera = new MelonEngine.Camera({aspectRatio: canvas.width/canvas.height});
+// Create objects
+const cubeMesh = new MelonEngine.Mesh(gl, vertices, colors, indices);
+const programInstance = new MelonEngine.ProgramInstance(gl); //default shaders invoked
+const cubeInstance = new MelonEngine.MeshInstance(cubeMesh, programInstance, renderer, [0, 0, 0], [0, 0, 0], [1, 1, 1]);
+
+
 
 let angle = 0;
 // Render loop
 function render() {
     angle += 0.015
     cubeInstance.updateModelMatrix([0,0,0], [0,angle,0],[1,1,1]);
-    rendererI.render(gl, camera); // Pass a camera instance
+    renderer.render(gl, camera); // Pass a camera instance
     requestAnimationFrame(render);
 }
 
---
+++ lab5/main.js
@@ -0,0 +1,25 @@
+const canvas = document.querySelector('canvas');
+const gl = initCanvas(canvas);
+gl.viewport(0, 0, canvas.width, canvas.height);
+gl.clearColor(0.6, 0.6, 0.6, 1.0);
+gl.enable(gl.DEPTH_TEST);
+gl.enable(gl.CULL_FACE);
+gl.cullFace(gl.BACK);
+
+const renderer = new MelonEngine.Renderer(gl);
+const camera = new MelonEngine.Camera({aspectRatio: canvas.width/canvas.height});
+const pyramidMesh = new MelonEngine.Mesh(gl, Pyramid.positions, Pyramid.colors, Pyramid.faces);
+const programInstance = new MelonEngine.ProgramInstance(gl); //invokes default mode
+const pyramid = new MelonEngine.MeshInstance(pyramidMesh, programInstance, renderer, [0, 0, 0], [0, 0, 0], [1, 1, 1]);
+
+function draw(time = 0) {
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    pyramid.updateModelMatrix(
+        vec3FromSliders(menu.position),
+        vec3ToRadians(vec3FromSliders(menu.rotation)),
+        vec3FromSliders(menu.scale)
+    );
+    renderer.render(gl, camera);
+    window.requestAnimationFrame(draw);
+}
+draw();
